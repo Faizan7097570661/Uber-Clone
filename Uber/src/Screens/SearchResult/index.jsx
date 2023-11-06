70b643ccd0b947cc2d6495ca631631e51de3e9db
Faizan7097570661 <135163460+Faizan7097570661@users.noreply.github.com>
2023-11-06 23:31:35 +0530

    Update index.jsx

--- Uber/src/Screens/SearchResult/index.jsx
+++ Uber/src/Screens/SearchResult/index.jsx
@@ -1,35 +1,27 @@
-<<<<<<< HEAD
 import { View, Text, Dimensions } from 'react-native'
-=======
 import { View, Text ,Dimensions} from 'react-native'
->>>>>>> 5d96259fbfd44e75bb2812a49582a44cb8d4998b
 import React from 'react'
 import UberTypes from '../../components/uberTypes'
-<<<<<<< HEAD
 import { useRoute } from '@react-navigation/native'
-=======
->>>>>>> 5d96259fbfd44e75bb2812a49582a44cb8d4998b
 import RouteMap from '../../components/RouteMap'
 
 const SearchResult = () => {
   const route=useRoute()
   const {destinationPlace,originPlace}=route.params
   return (
-<<<<<<< HEAD
     <View style={{display:'flex',justifyContent:'space-between'}}>
     <View style={{height:Dimensions.get('window').height-400}}>
     <RouteMap origin={origin} destination={destination}/>
     </View>
     <View style={{height:400}}>
-=======
     <View style={{display:'flex'}}>
 <View style={{height:Dimensions.get('window').height-400}}>
     <RouteMap/>
 </View>
 <View style={{height:400}}>
->>>>>>> 5d96259fbfd44e75bb2812a49582a44cb8d4998b
     <UberTypes/>
 </View>
+    </View>
     </View>
     </View>
   )
